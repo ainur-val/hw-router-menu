ecd7ead4359831ab7ce9419b08175a19a4137d10
Aynur Valiev <valiev.ainur.1991@gmail.com>
2025-08-16 11:43:13 +0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import './App.css'
+import Menu from './components/Menu';
+import HomePage from './components/HomePage';
+import DriftPage from './components/DriftPage';
+import ForzaPage from './components/ForzaPage';
+import TimeAttackPage from './components/TimeAttackPage';
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <Menu />
+        <div className="page">
+          <Routes>
+          <Route path="/" exact element={< HomePage />} />
+          <Route path="/drift" element={< DriftPage />} />
+          <Route path="/timeattack" element={< TimeAttackPage />} />
+          <Route path="/forza" element={< ForzaPage />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
